92cc7a65303888abb9f18e3633d39b1b1563c6a3
Max Arthur <dgjabour@gmail.com>
2026-09-21 15:14:10 -0300

    Contrato da superclasse Obra

---
+++ include/Obra.hpp
@@ -0,0 +1,28 @@
+#ifndef OBRA_H
+#define OBRA_H
+
+#include <iostream>
+#include <string>
+
+class Obra {
+    private:
+    std::string titulo, autor;
+    int id;
+    bool disponivel, danificado;
+
+    public:
+    Obra(std::string t, std::string a, int id);
+    void setDisponivel(bool d);
+    void setDanificado(bool d);
+
+    std::string getTitulo() const;
+    std::string getAutor() const;
+    int getID() const;
+    bool isDisponivel() const;
+    bool isDanificado() const;
+
+    virtual void printObra() const;
+
+    virtual ~Obra() = default;
+};
+#endif
