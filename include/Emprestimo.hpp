607a216b2a376b2ec6feb9a5136aed2242444067
Max Arthur <dgjabour@gmail.com>
2026-09-21 21:24:23 -0300

    Contrato da classe Emprestimo

---
+++ include/Emprestimo.hpp
@@ -0,0 +1,28 @@
+#ifndef EMPRESTIMO_H
+#define EMPRESTIMO_H
+
+#include <iostream>
+#include "Obra.hpp"
+#include "Pessoa.hpp"
+
+class Emprestimo {
+    private:
+    Obra *obra;
+    Pessoa *usuario;
+    int retirada, devolucao;
+    bool ativo;
+
+    public:
+    Emprestimo(Obra &o, Pessoa &p, int data);
+
+    Obra& getObra() const;
+    Pessoa& getUsuario() const;
+    int getRetirada() const;
+    int getDevolucao() const;
+    bool isAtivo() const;
+
+    int getAtraso(int data);
+    void renovarEmprestimo(int data);
+    bool validarEmprestimo();
+};
+#endif
