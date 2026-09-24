8a686f0ff667fa59cd779bf8a6aada0512f1640b
Max Arthur <dgjabour@gmail.com>
2026-09-24 13:20:04 -0300

    Adicionando protected em Obra.hpp

--- include/Emprestimo.hpp
+++ include/Emprestimo.hpp
@@ -7,24 +7,24 @@
 
 class Emprestimo {
     private:
-    Obra *obra;
-    Pessoa *usuario;
-    int retirada, devolucao;
-    bool ativo;
+        Obra *obra;
+        Pessoa *usuario;
+        int retirada, devolucao;
+        bool ativo;
 
     public:
-    Emprestimo(Obra &o, Pessoa &p, int data);
+        Emprestimo(Obra &o, Pessoa &p, int data);
 
-    Obra& getObra() const;
-    Pessoa& getUsuario() const;
-    int getRetirada() const;
-    int getDevolucao() const;
-    bool isAtivo() const;
+        Obra& getObra() const;
+        Pessoa& getUsuario() const;
+        int getRetirada() const;
+        int getDevolucao() const;
+        bool isAtivo() const;
 
-    int getAtraso(int data);
-    void renovarEmprestimo(int data);
-    bool validarEmprestimo();
+        int getAtraso(int data);
+        void renovarEmprestimo(int data);
+        bool validarEmprestimo();
 
-    ~Emprestimo() = default;
+        ~Emprestimo() = default;
 };
 #endif
--- include/Obra.hpp
+++ include/Obra.hpp
@@ -6,23 +6,24 @@
 
 class Obra {
     private:
-    std::string titulo, autor;
-    int id;
-    bool disponivel, danificado;
+        std::string titulo, autor;
+        int id;
+        bool disponivel, danificado;
+
+    protected:
+        virtual void printObra() const = 0;
 
     public:
-    Obra(std::string t, std::string a, int id);
-    void setDisponivel(bool d);
-    void setDanificado(bool d);
+        Obra(std::string t, std::string a, int id);
+        void setDisponivel(bool d);
+        void setDanificado(bool d);
 
-    std::string getTitulo() const;
-    std::string getAutor() const;
-    int getID() const;
-    bool isDisponivel() const;
-    bool isDanificado() const;
+        std::string getTitulo() const;
+        std::string getAutor() const;
+        int getID() const;
+        bool isDisponivel() const;
+        bool isDanificado() const;
 
-    virtual void printObra() const = 0;
-
-    virtual ~Obra() = default;
+        virtual ~Obra() = default;
 };
 #endif
