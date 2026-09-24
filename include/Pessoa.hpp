e10289475cd1d8b6cd032786937cbaa6aa8053a7
Max Arthur <dgjabour@gmail.com>
2026-09-24 14:23:42 -0300

    Contrato da superclasse Pessoa

---
+++ include/Pessoa.hpp
@@ -0,0 +1,29 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <iostream>
+#include <string>
+
+class Obra;
+class Emprestimo;
+
+class Pessoa {
+    private:
+        std::string nome;
+        std::string CPF;
+
+    public:
+        Pessoa(std::string n, std::string c);
+
+        std::string getNome() const;
+        std::string getCPF() const;
+        virtual int getPrazo() const;
+
+        virtual bool podeEmprestar(const Obra *o) const;
+        Emprestimo** consultarAtivos();
+        Emprestimo** historicoEmprestimos();
+        void pedirRenovacao(Emprestimo *e);
+
+        virtual ~Pessoa() = default;
+};
+#endif
